141247f18169cc030cd8445815aca6826d07af19
dahamv <dahamviharana@gmail.com>
2019-08-23 08:16:10 +0000

    removed tutorial stuff from master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,4 @@ import { Component } from "@angular/core";
     moduleId: module.id,
     templateUrl: "./app.component.html"
 })
-export class AppComponent {
-    constructor() {
-        console.log("Hello World 1");
-        console.log(JSON.stringify({
-            type: 'LOG_TO_CONSOLE',
-            payload: 'Hello World 2'
-        }));
-
-    }
- }
+export class AppComponent {}

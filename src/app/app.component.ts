1780e37f082d9a0716950bb726c951c373f39ea4
dahamv <dahamviharana@gmail.com>
2019-08-23 08:05:46 +0000

    following groseries tutorial:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,13 @@ import { Component } from "@angular/core";
     moduleId: module.id,
     templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent {
+    constructor() {
+        console.log("Hello World 1");
+        console.log(JSON.stringify({
+            type: 'LOG_TO_CONSOLE',
+            payload: 'Hello World 2'
+        }));
+
+    }
+ }
